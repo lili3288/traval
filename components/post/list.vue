0871979c82ba7e4d19ff0c9834003798ac905c3b
诸葛建国 <328836722@qq.com>
2019-09-08 19:56:33 +0800

    完成文章测试

--- components/post/list.vue
+++ components/post/list.vue
@@ -1,24 +1,134 @@
 <template>
   <div class="list">
-    <el-row type="flex" class="title" justify="space-between">
+    <el-row type="flex" class="top" justify="space-between">
       <div class="left">推荐攻略</div>
       <el-button type="primary">主要按钮</el-button>
     </el-row>
+    <div class="composition">
+      <div class="inlist" v-for="(item,index) in compositions" :key="index">
+        <!-- 标题 -->
+        <div class="title">{{item.title}}</div>
+        <!-- 摘要 -->
+        <div class="summary" v-html="item.summary" ></div>
+        <!-- 图片 -->
+        <el-row type="flex" class="photo" justify="space-between">
+          <div class="photo-list" v-for="(item,index) in item.images" :key="index">
+            <img
+              :src="item"
+              alt
+            />
+          </div>
+         
+        </el-row>
+        <el-row type="flex" class="read" justify="space-between">
+          <div class="read-left">
+            <i class="el-icon-location-outline"></i>
+            {{item.cityName}}
+            <span class="by">by</span>
+            <img
+              :src="`${$axios.defaults.baseURL}${item.account.defaultAvatar}`"
+              alt
+            />
+            <span class="nickname">{{item.account.nickname}}</span>
+            <i class="el-icon-view"></i>
+            {{item.watch}}
+          </div>
+          <div class="read-right">{{item.like}}
+            <span>赞</span>
+          </div>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      cityName: "",
+      compositions: []
+    };
+  },
+  mounted() {
+    // 获取文章列表
+    this.$axios({
+      url: "/posts"
+      // params:{city:this.cityName}
+    }).then(res => {
+      console.log(res);
+      this.compositions = res.data.data;
+      console.log(this.composition);
+    });
+  }
+};
 </script>
 <style lang="less" scoped>
-.list{
-    .title{
-        align-items: center;
-        padding: 10px 0;
-        border-bottom: 1px solid #ddd;
-        .left{
-
-        }
+.list {
+  width: 700px;
+  .top {
+    align-items: center;
+    padding: 10px 0;
+    border-bottom: 1px solid #ddd;
+    width: 700px;
+    .left {
     }
+  }
+  // 列表
+  .composition {
+    .inlist {
+      border-bottom: 1px solid skyblue;
+      padding: 20px 0;
+      .title {
+        font-size: 18px;
+        color: #333;
+      }
+      .summary {
+        height: 58px;
+        overflow: hidden;
+        margin: 10px 0;
+        color: #666;
+        font-size: 14px;
+      }
+      .photo {
+        width: 100%;
+        .photo-list {
+          
+          img {
+            width: 220px;
+            height: 150px;
+          }
+        }
+      }
+      // 文章周边信息
+      .read {
+        margin-top: 10px;
+        .read-left {
+   
+          font-size: 12px;
+          color: #999;
+          img {
+            width: 16px;
+            height: 16px;
+            vertical-align: middle;
+          }
+          .by{
+            margin: 0 5px;
+          }
+          .nickname{
+            color: orange;
+            margin: 0 5px;
+          }
+        }
+        .read-right {
+          color: orange;
+          font-size: 16px;
+          span{
+            margin-left: 5px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -12,72 +12,32 @@
       </div>
       <el-button slot="reference">{{item.type}}</el-button>
     </el-popover>-->
-    <el-row type="flex" :gutter="50">
+    <el-row type="flex" :gutter="40" >
       <!-- 左侧 -->
-      <el-col :span="6">
+      <el-col :span="7">
         <!-- 推荐城市列表 -->
-        <div class="recom">
-          <div class="recom-list" v-for="(item,index) in recomCity" :key="index"   @mouseover="showContent(index)"
-           @mouseout="hideContent">
-            <div
-              class="recom-list-each"
-              :class="{noborder:index===isShow}"
-            
-            >
-              <span>{{item.type}}</span>
-              <i class="el-icon-arrow-right"></i>
-            </div>
-            <!-- 隐藏详情 -->
-            <div class="recom-content" :class="{active:index===isShow}">
-              <div class="content-list" v-for="(value,num) in item.children" :key="num">
-                <span>{{num+1}}</span>
-                <i>{{value.city}}</i>
-                {{value.desc}}
-              </div>
-            </div>
-          </div>
-        </div>
+        <recomcity />
       </el-col>
       <!-- 右侧 -->
-      <el-col :span="18">
+      <el-col :span="17">
+        <!-- 搜索栏 -->
         <search></search>
-        <list/>
+        <!-- 文章列表 -->
+        <list />
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import search from '@/components/post/search'
-import list from '@/components/post/list'
+import search from "@/components/post/search";
+import list from "@/components/post/list";
+import recomcity from "@/components/post/recomcity";
 export default {
-  data() {
-    return {
-      recomCity: [],
-      isShow: -1
-    };
-  },
-  components:{
-    search,list
-  },
-  mounted() {
-    // 获取推荐
-    this.$axios({
-      url: "/posts/cities"
-    }).then(res => {
-      console.log(res);
-      if (res.request.status === 200) {
-        this.recomCity = res.data.data;
-      }
-    });
-  },
-  methods: {
-    showContent(index) {
-      this.isShow = index;
-    },
-    hideContent(){
-       this.isShow =-1;
-    }
+  components: {
+    search,
+    list,
+    recomcity
   }
 };
 </script>
@@ -88,76 +48,5 @@ export default {
   margin: 0 auto;
   margin-bottom: 500px;
   padding: 20px 0;
-  .recom {
-    border: 1px solid #ddd;
-    border-right: 0;
-    width: 240px;
-    position: relative;
-    // box-sizing: border-box;
-    .recom-list {
-      height: 40px;
-
-      // box-sizing: border-box;
-
-      line-height: 40px;
-      border-bottom: 1px solid #ddd;
-      
-      .recom-list-each {
-        background-color: #fff;
-        font-size: 14px;
-
-        border-right: 1px solid #ddd;
-        position: relative;
-        padding: 0 20px;
-        // box-sizing: border-box;
-        z-index: 10;
-
-        i {
-          position: absolute;
-          height: 40px;
-          line-height: 40px;
-          right: 10px;
-          top: 0;
-        }
-      }
-      .noborder {
-        border-right: 1px solid rgba(209, 49, 49, 0);
-        color: orange;
-      }
-      &:last-child {
-        border-bottom: 0;
-      }
-      .recom-content {
-        border: 1px solid #ddd;
-        z-index: 6;
-        background-color: #fff;
-        position: absolute;
-        left: 239px;
-        // box-sizing: border-box;
-        top: -1px;
-        display: none;
-        width: 300px;
-        .content-list {
-          height: 40px;
-          line-height: 40px;
-          padding-left: 20px;
-          font-size: 12px;
-          color: #999;
-          span {
-            font-size: 30px;
-            color: orange;
-          }
-          i {
-            font-size: 16px;
-            color: orange;
-            margin: 0 10px;
-          }
-        }
-      }
-      .active {
-        display: block;
-      }
-    }
-  }
 }
 </style>
